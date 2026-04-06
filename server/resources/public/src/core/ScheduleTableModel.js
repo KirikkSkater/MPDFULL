3a495c80abeaacbca36da46f7004bb38a26d9141
malkov_ke <Kirill.Malkov@yakovlev.ru>
2026-04-06 14:40:23 +0300

    duration

--- server/resources/public/src/core/ScheduleTableModel.js
+++ server/resources/public/src/core/ScheduleTableModel.js
@@ -2557,6 +2557,12 @@ addTaskDuration(rowIndex) {
     const taskDuration = doc.createElement('taskDuration');
     taskDuration.setAttribute('procedureDuration', newDuration.procedureDuration);
     taskDuration.setAttribute('startupDuration', newDuration.startupDuration);
+
+    taskDuration.setAttribute('closeupDuration', "-");
+    taskDuration.setAttribute('procedureDuration', "0");
+    taskDuration.setAttribute('unitOfMeasure', "ч.ч.");
+
+
     taskDurationPmd.appendChild(taskDuration);
 
     // Находим все productionMaintData элементы
